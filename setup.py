5e6024ab8a537f651122804568b8fc11be92b225
Anrijs <anrijs@anrijs.lv>
2024-05-07 09:25:37 +0300

    v1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mikrotik-bt5",
-    version="1.0.1",
+    version="1.0.2",
     description="MikroTik BT5 scanner",
     long_description=long_description,
     long_description_content_type="text/markdown",
